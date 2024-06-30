ec3fd1ec35a18430deab15f9ea283a17213d8443
prisoneroflove <ashwinshine.nonu@gmail.com>
2024-06-30 23:04:02 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<center><iframe src='https://my.spline.design/molang3dcopy-420a860d7b74200e2f15cef6d5d6cdfc/' frameborder='0' width='968' height='679' title="rocket"></iframe></center>
+
+<h1>Watch out: This is a construction area</h1>
+<h3>Nah kidding, this is a decoy; check out other pages</h3>
+
+<nav>
+    <a href="/blog">Blog</a>
+</nav>
---
+++ src/routes/blog/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/blog">Home</a>
+    <a href="/blog/projects">Projects</a>
+    <a href="/blog/event">Events</a>
+</nav>
+<slot></slot>
